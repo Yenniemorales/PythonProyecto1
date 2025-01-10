98e3b0b27b9beeda1a74c512c85a83bf529c31fb
YennieMoralesRoco <yennie98mr@live.cl>
2025-01-09 21:27:55 -0300

    Renombrado del proyecto de Proyecto1 a MiProyecto

--- MiProyecto/urls.py
+++ MiProyecto/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Proyecto1 project.
+URL configuration for MiProyecto project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
